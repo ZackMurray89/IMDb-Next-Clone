73b52bb69b850e507ba3f3c4ff56db4f1fb9e8a7
ZackMurray89 <zackmurray.dev@gmail.com>
2025-01-29 15:05:10 -0600

    Mongo Fix 3

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -11,10 +11,7 @@ export const connect = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     initialized = true;
 
